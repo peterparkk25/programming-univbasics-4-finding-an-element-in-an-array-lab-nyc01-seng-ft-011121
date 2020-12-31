a43150ba8196b9a51d058c4865eedb968e33aede
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-31 23:21:05 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,5 +5,7 @@ def find_element_index(array, value_to_find)
    if array_test == TRUE
       array.index(value_to_find)
    elsif array_test == FALSE
-      
+      array_test = NIL
+      array_test
+   end
 end
